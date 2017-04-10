2aeb7d619834596fc188190a08de7579e06c15a3
NirsaGg <irademyanyuk@gmail.com>
2017-04-10 19:20:19 +0300

    add script for toggle menu

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var toggleBtn = document.querySelector(".toggle-btn");
+var menu = document.querySelector(".menu__flex-inner--closed");
+
+toggleBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (menu.classList.contains("menu__flex-inner--opened")) {
+    menu.classList.remove("menu__flex-inner--opened");
+  } else {
+    menu.classList.add("menu__flex-inner--opened");
+  }
+});
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+      if (menu.classList.contains("menu__flex-inner--opened")) {
+        menu.classList.remove("menu__flex-inner--opened");
+      }
+  }
+});
