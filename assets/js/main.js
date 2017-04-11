e04d87e438703f02aed81067d43d5494c57ae215
4ykis <4yk.roman@gmail.com>
2017-04-11 14:36:28 +0300

    some commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,33 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded',function(){
 
-var toggleBtn = document.querySelector(".toggle-btn");
-var menu = document.querySelector(".menu__flex-inner--closed");
+    const toggleBtn = document.querySelector(".toggle-btn");
+    const menu = document.querySelector(".menu__flex-inner--closed");
 
-toggleBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  if (menu.classList.contains("menu__flex-inner--opened")) {
-    menu.classList.remove("menu__flex-inner--opened");
-  } else {
-    menu.classList.add("menu__flex-inner--opened");
-  }
-});
-
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-      if (menu.classList.contains("menu__flex-inner--opened")) {
-        menu.classList.remove("menu__flex-inner--opened");
-      }
-  }
-});
+    toggleBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (menu.classList.contains("menu__flex-inner--opened")) {
+            menu.classList.remove("menu__flex-inner--opened");
+        } else {
+            menu.classList.add("menu__flex-inner--opened");
+        }
+    });
 
 
+    window.addEventListener("keydown", function (event) {
+        if (event.keyCode === 27) {
+            if (menu.classList.contains("menu__flex-inner--opened")) {
+                menu.classList.remove("menu__flex-inner--opened");
+            }
+        }
+    });
 
-  $('.js-slider').slick({
-    fade:true,
-        infinite:true,
-        arrows:false,
+
+    $('.js-slider').slick({
+        fade: true,
+        infinite: true,
+        arrows: false,
         autoplay: true,
         autoplaySpeed: 3000,
-  });
+    });
 
 });
