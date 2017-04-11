c368cbacee222bbbbab2d09e6a0b7bffc603cdf6
4ykis <4yk.roman@gmail.com>
2017-04-11 14:43:19 +0300

    change JS to Object oriented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,47 @@
-document.addEventListener('DOMContentLoaded',function(){
+const front = {
+    MobileMenuOpenClose: function () {
+        let toggleBtn = document.querySelector(".toggle-btn");
+        let menu = document.querySelector(".menu__flex-inner--closed");
 
-    const toggleBtn = document.querySelector(".toggle-btn");
-    const menu = document.querySelector(".menu__flex-inner--closed");
-
-    toggleBtn.addEventListener("click", function (event) {
-        event.preventDefault();
-        if (menu.classList.contains("menu__flex-inner--opened")) {
-            menu.classList.remove("menu__flex-inner--opened");
-        } else {
-            menu.classList.add("menu__flex-inner--opened");
-        }
-    });
-
-
-    window.addEventListener("keydown", function (event) {
-        if (event.keyCode === 27) {
+        toggleBtn.addEventListener("click", function (event) {
+            event.preventDefault();
             if (menu.classList.contains("menu__flex-inner--opened")) {
                 menu.classList.remove("menu__flex-inner--opened");
+            } else {
+                menu.classList.add("menu__flex-inner--opened");
             }
+        });
+
+        window.addEventListener("keydown", function (event) {
+            if (event.keyCode === 27) {
+                if (menu.classList.contains("menu__flex-inner--opened")) {
+                    menu.classList.remove("menu__flex-inner--opened");
+                }
+            }
+        });
+    },
+    Slider:{
+        mainSlider:{
+            fade: true,
+            infinite: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        },
+        SLICK:function (params) {
+            $('.js-slider').slick(params);
+        },
+        init:function () {
+            this.SLICK(this.mainSlider);
         }
-    });
+    },
+    init: function () {
+        this.MobileMenuOpenClose();
+        this.Slider.init();
+    }
+};
 
 
-    $('.js-slider').slick({
-        fade: true,
-        infinite: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    });
-
+document.addEventListener('DOMContentLoaded',function(){
+    front.init();
 });
