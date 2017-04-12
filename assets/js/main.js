4da33b71cd8430aff751f78b7b5846c7f52a94f9
NirsaGg <irademyanyuk@gmail.com>
2017-04-12 20:43:46 +0300

    add featured and feedback blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,3 +120,19 @@ const front = {
 document.addEventListener('DOMContentLoaded', function () {
     front.init();
 });
+
+function setEqualHeight(columns) {
+  var tallestcolumn = 0;
+  columns.each(
+    function() {
+      currentHeight = $(this).height();
+      if (currentHeight > tallestcolumn) {
+        tallestcolumn = currentHeight;
+      }
+    }
+  );
+  columns.height(tallestcolumn);
+}
+$(document).ready(function() {
+  setEqualHeight($(".feedback__item > div"));
+});
